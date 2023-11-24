6f8033f7d095e21100c6fe6676650cc205aaf405
kitodev <tkornel19@gmail.com>
2023-11-24 16:13:45 +0100

    fix error

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LotteryGame from './Components/Lotto';
 import OperatorDashboard from './Components/OperatorDashboard';
 
-const App: React.FC = () => {
+const App = () => {
   return (
-    <Router>
       <Routes>
         <Route path="/" element={<LotteryGame />} />
         <Route path="/operator-dashboard" element={<OperatorDashboard />} />
       </Routes>
-    </Router>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom/client";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-// import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
 
